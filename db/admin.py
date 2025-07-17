4bff6ac79532afd67940c8bf6c28aa5f345544d8
DESKTOP-3KUFC8D\nypzl <nyshtoprivet@gmail.com>
2025-07-17 19:20:17 +0300

    add new models + migration

--- db/admin.py
+++ db/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import FootballFederationM
+from .models import CountryFootball, FootballFederationM,FootballLeague,FootballCompetition
 
 # Register your models here.
 admin.site.register(FootballFederationM)
+admin.site.register(CountryFootball)
+admin.site.register(FootballLeague)
+admin.site.register(FootballCompetition)
--- db/models.py
+++ db/models.py
@@ -26,7 +26,35 @@ class FootballFederationM(models.Model):
 # TODO: ограничение для Member, Associate_Member - только положительные числа
 
 
-class FootballAssociate(models.Model):
-    pass
+class CountryFootball(models.Model):
+    Country_Name = models.CharField()
+    Tournament_In_Country = models.ManyToManyField(
+        'FootballCompetition',
+        related_name='countries'
+    )
+    class Meta():
+        verbose_name = "Country"
+        verbose_name_plural = "Countries"
+    
+    def __str__(self):
+        return self.Country_Name
 
 
+class FootballLeague(models.Model):
+    League_Name = models.CharField()
+    Country_League = models.CharField() # желательно хранить страну, хотя лучше подумать над этим
+    Count_Team = models.IntegerField()
+    def __str__(self):
+        return self.LeagueName
+
+
+class FootballCompetition(models.Model):
+    Competition_Name = models.CharField()
+    def __str__(self):
+        return self.Competition_Name
+
+
+class FootballTeam(models.Model):
+    Team_Name = models.CharField()
+    def __str__(self):
+        return self.Team_Name
